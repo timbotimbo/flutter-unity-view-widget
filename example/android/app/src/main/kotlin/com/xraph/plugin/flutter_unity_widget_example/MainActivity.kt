31eacb9af7dad331ecb349f7fc1827df3778527a
timbotimbo <timbotimbo@users.noreply.github.com>
2023-07-23 23:16:27 +0200

    Add @keep to prevent mUnityPlayer being stripped in release builds.

--- example/android/app/src/main/kotlin/com/xraph/plugin/flutter_unity_widget_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/xraph/plugin/flutter_unity_widget_example/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.xraph.plugin.flutter_unity_widget_example
 
 import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.Keep;
 
 class MainActivity: FlutterActivity() {
 
     // replicates mUnityPlayer like in UnityActivity, as AndroidJavaProxy expects it on the activity.
-    @JvmField
+    @Keep @JvmField 
     var mUnityPlayer: java.lang.Object? = null 
 }
